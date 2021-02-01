ff14786fc185a16b1d084630a487a509f5f789be
elias-cardon <62133614+elias-cardon@users.noreply.github.com>
2021-02-01 15:45:15 +0100

    Rangement du code

--- js/tweaks.js
+++ js/tweaks.js
@@ -1,49 +1,47 @@
-$( function() {
+$(function () {
 
     //jquery ui
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
-    
-    // add active class to li element
-    $( ".list-group-item" ).hover(
-      function() {
-      $(this).addClass("active");
-  
-    },
-      function() {
-      
-      $(this).removeClass("active");
+    $("#sortable").sortable();
+    $("#sortable").disableSelection();
 
-    });
+    // add active class to li element
+    $(".list-group-item").hover(
+        function () {
+            $(this).addClass("active");
+
+        },
+        function () {
+
+            $(this).removeClass("active");
+
+        });
 
     //Prevent submit if input is empty
 
-    $ ("#submit").click(function() {
+    $("#submit").click(function () {
 
-      if ($("#todo").val() == "" ) {
+        if ($("#todo").val() == "") {
 
-        $("#todo").attr
-        (
-          "placeholder", "Give your task a name e.g. laundry, exams, etc..."
-        )
-        $("#todo").css("border-bottom", "2px solid red");
-        return false;
-      }
+            $("#todo").attr
+            (
+                "placeholder", "Give your task a name e.g. laundry, exams, etc..."
+            )
+            $("#todo").css("border-bottom", "2px solid red");
+            return false;
+        }
 
     });
 
     //dismiss alerts and autofocus input
-    $(".close").click(function() {
+    $(".close").click(function () {
 
-      $("div[role='alert']").hide();
-      $("hr").hide();
+        $("div[role='alert']").hide();
+        $("hr").hide();
 
-      $("#todo").focus();
+        $("#todo").focus();
 
 
     });
-    
-    
 
 
 });
